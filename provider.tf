5d11ed440e79432ac68aa9d58cdb7716305ea52a
Bruna Gois <bruna-chan@live.com>
2024-05-21 16:24:26 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.48.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops6550a180"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
